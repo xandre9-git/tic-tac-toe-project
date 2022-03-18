d5976971faa6aac82f6abf88a34aefdece3b56ed
xandre9 <xandre9@xandre9.xandre9>
2022-03-18 07:27:42 -0400

    Rearranged 'gameBoardObject()' functions to top of program, modified 'vsPlayerBtn' event listener to not use a nested event listener from 'selectedBox()' which got player names and made X or O marks on board. Created 'nameGetter()' instead to get player names and 'getPlayerNames()' to set names throughout the program. Created 'playerTick()' which is used as a click event listener for '.game-board-box' tiles to mark letter on gameboard. Created 'resetGameBoardBoxes()' to reduce repetition of code with 'selectedBoxes' reset.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-
-
 // gameboard object
 
 // You’re going to store the gameboard as an array inside of a Gameboard object, so start there! Your players are also going to be stored in objects… and you’re probably going to want an object to control the flow of the game itself.
@@ -9,107 +7,7 @@
 const gameBoardObject = () => {
   "use strict";
   (function () {
-    // Body
-    const body = document.querySelector("body");
-
-    // Title
-    const gameTitleH1 = document.createElement("h1");
-    gameTitleH1.className = "game-title";
-    gameTitleH1.textContent = "Tic Tac Toe";
-    body.append(gameTitleH1);
-
-    // Game Options
-    const gameOptionsDiv = document.createElement("div");
-    gameOptionsDiv.className = "game-options";
-
-    // Versus Player Button
-    const vsPlayerBtn = document.createElement("button");
-    vsPlayerBtn.type = "button";
-    vsPlayerBtn.className = "option-button";
-    vsPlayerBtn.textContent = "Versus Human";
-    vsPlayerBtn.addEventListener("click", () => {
-      playersObj = players();
-      if (playersObj === undefined) {
-        return;
-      }
-      gameOptionsDiv.className = "game-status";
-      gameOptionsDiv.textContent = `${playersObj.player1} vs. ${playersObj.player2}`;
-      selectedBox(playersObj.player1, playersObj.player2);
-      return playersObj;
-    });
-    gameOptionsDiv.append(vsPlayerBtn);
-
-    // Versus Computer Button
-    const vsCpuBtn = document.createElement("button");
-    vsCpuBtn.className = "option-button";
-    vsCpuBtn.textContent = "Versus Computer";
-    // gameOptionsDiv.append(vsCpuBtn);
-    body.append(gameOptionsDiv);
-
-    // Game Board Container
-    const gameBoardDiv = document.createElement("div");
-    gameBoardDiv.className = "game-board";
-    body.append(gameBoardDiv);
-    const GameBoardBox = (parent, i) => {
-      const box = document.createElement("div");
-      box.className = "game-board-box";
-      box.dataset.indexNumber = i;
-      return parent.append(box);
-    };
-    const gameBoard = () => {
-      for (let j = 0; j < 9; j++) {
-        GameBoardBox(gameBoardDiv, j);
-      }
-    };
-    gameBoard();
-
-    // Restart Function
-    const restartGame = () => {
-      xScore = [];
-      oScore = [];
-      checkArray(winCodes, xScore);
-      checkArray(winCodes, oScore);
-      let selectedBoxes = Array.from(
-        document.querySelectorAll(".game-board-box")
-      );
-      selectedBoxes.forEach((e) => {
-        e.textContent = "";
-      });
-      gameOptionsDiv.textContent = `${playersObj.player1} vs. ${playersObj.player2}`;
-    };
-
-    // Restart Button
-    const restartBtn = document.createElement("button");
-    restartBtn.type = "button";
-    restartBtn.className = "option-button";
-    restartBtn.textContent = "Restart";
-    restartBtn.addEventListener("click", restartGame);
-    body.append(restartBtn);
-
-    // New Game Section
-    const newGame = () => {
-      xScore = [];
-      oScore = [];
-      checkArray(winCodes, xScore);
-      checkArray(winCodes, oScore);
-      let selectedBoxes = Array.from(
-        document.querySelectorAll(".game-board-box")
-      );
-      selectedBoxes.forEach((e) => {
-        e.textContent = "";
-      });
-      gameOptionsDiv.textContent = "";
-      gameOptionsDiv.className = "game-options";
-      gameOptionsDiv.append(vsPlayerBtn);
-      // gameOptionsDiv.append(vsCpuBtn);
-    };
-    const newGameBtn = document.createElement("button");
-    newGameBtn.type = "button";
-    newGameBtn.className = "option-button";
-    newGameBtn.textContent = "New Game";
-    newGameBtn.addEventListener("click", newGame);
-    body.append(newGameBtn);
-
+    
     // Reference Check Function
     function isPartOf(ref, arg) {
       return arg.every(function (element) {
@@ -166,56 +64,21 @@ const gameBoardObject = () => {
     };
     const toggle = tickToggle();
 
-    // Box Selection
-    const selectedBox = (player1, player2) => {
-      let selectedBoxes = Array.from(
-        document.querySelectorAll(".game-board-box")
-      );
-      selectedBoxes.forEach((e) => {
-        e.addEventListener("click", () => {
-          // check to see if value is already there
-          if (e.textContent == "X" || e.textContent == "O") {
-            return;
-          }
-          if (
-            gameOptionsDiv.textContent == `${player1} wins!` ||
-            gameOptionsDiv.textContent == `${player2} wins!`
-          ) {
-            return;
-          } else {
-            let letterSwap = toggle();
-            let res = e.getAttribute("data-index-number");
-            e.textContent = letterSwap;
-            // push player response into an array
-            if (e.textContent == "X") {
-              // 'res' had to be converted from a string to a number for checkArray() to work
-              xScore.push(parseInt(res));
-              checkArray(winCodes, xScore);
-              if (checkArray(winCodes, xScore) === true) {
-                gameOptionsDiv.textContent = `${player1} wins!`;
-                letterSwap = toggle();
-              }
-            } else {
-              oScore.push(parseInt(res));
-              checkArray(winCodes, oScore);
-              if (checkArray(winCodes, oScore) === true) {
-                gameOptionsDiv.textContent = `${player2} wins!`;
-              }
-            }
-            if (
-              oScore.length === 4 &&
-              xScore.length === 5 &&
-              !checkArray(winCodes, xScore)
-            ) {
-              gameOptionsDiv.textContent = `Draw!`;
-            }
-          }
-        });
-      });
-    };
-    
-    // Players Data
-    let players = () => {
+    // Body
+    const body = document.querySelector("body");
+
+    // Title
+    const gameTitleH1 = document.createElement("h1");
+    gameTitleH1.className = "game-title";
+    gameTitleH1.textContent = "Tic Tac Toe";
+    body.append(gameTitleH1);
+
+    // Game Options
+    const gameOptionsDiv = document.createElement("div");
+    gameOptionsDiv.className = "game-options";
+
+    // Player Names Setter Function
+    const getPlayerNames = () => {
       let player1 = prompt("Enter Player X's name (X Goes First).");
       if (player1 === null) {
         return;
@@ -226,7 +89,151 @@ const gameBoardObject = () => {
       }
       return { player1, player2 };
     };
+
     let playersObj = {};
+
+    // Name Getter Function
+    function nameGetter(){
+      playersObj = getPlayerNames();
+      if (playersObj === undefined) {
+        return;
+      }
+      gameOptionsDiv.className = "game-status";
+      gameOptionsDiv.textContent = `${playersObj.player1} vs. ${playersObj.player2}`;
+      return playersObj;
+    }
+
+    // Versus Player Button
+    const vsPlayerBtn = document.createElement("button");
+    vsPlayerBtn.type = "button";
+    vsPlayerBtn.className = "option-button";
+    vsPlayerBtn.textContent = "Versus Human";
+    vsPlayerBtn.addEventListener("click", function(){
+      playersObj = nameGetter();
+      console.log(playersObj);
+      return playersObj;
+    });
+    gameOptionsDiv.append(vsPlayerBtn);
+
+    // Versus Computer Button
+    const vsCpuBtn = document.createElement("button");
+    vsCpuBtn.className = "option-button";
+    vsCpuBtn.textContent = "Versus Computer";
+    // gameOptionsDiv.append(vsCpuBtn); (have not implemented ai coding as of yet)
+    body.append(gameOptionsDiv);
+
+    // Box Selection
+    const playerTick = (e, player1, player2) => {
+      // check to see if value is already there
+      if (e.textContent == "X" || e.textContent == "O") {
+        return;
+      }
+      if (
+        gameOptionsDiv.textContent == `${player1} wins!` ||
+        gameOptionsDiv.textContent == `${player2} wins!`
+      ) {
+        return;
+      } else {
+        let letterSwap = toggle();
+        let res = e.getAttribute("data-index-number");
+        e.textContent = letterSwap;
+        // push player response into an array
+        if (e.textContent == "X") {
+          // 'res' had to be converted from a string to a number for checkArray() to work
+          xScore.push(parseInt(res));
+          checkArray(winCodes, xScore);
+          if (checkArray(winCodes, xScore) === true) {
+            gameOptionsDiv.textContent = `${player1} wins!`;
+            letterSwap = toggle();
+          }
+        } else {
+          oScore.push(parseInt(res));
+          checkArray(winCodes, oScore);
+          if (checkArray(winCodes, oScore) === true) {
+            gameOptionsDiv.textContent = `${player2} wins!`;
+          }
+        }
+        if (
+          oScore.length === 4 &&
+          xScore.length === 5 &&
+          !checkArray(winCodes, xScore)
+        ) {
+          gameOptionsDiv.textContent = `Draw!`;
+        }
+      }
+    };
+
+    // Game Board Container
+    const gameBoardDiv = document.createElement("div");
+    gameBoardDiv.className = "game-board";
+    body.append(gameBoardDiv);
+
+    const GameBoardBox = (parent, i) => {
+      const box = document.createElement("div");
+      box.className = "game-board-box";
+      box.dataset.indexNumber = i;
+      box.textContent = null;
+      box.addEventListener("click", function(){
+        playerTick(box, playersObj.player1, playersObj.player2);
+      });
+      return parent.append(box);
+    };
+
+    const gameBoard = () => {
+      for (let j = 0; j < 9; j++) {
+        GameBoardBox(gameBoardDiv, j);
+      }
+    };
+    gameBoard();
+
+    // Game Board Box Reset Function
+    const resetGameBoardBoxes = () => {
+      let selectedBoxes = Array.from(
+        document.querySelectorAll(".game-board-box")
+      );
+      selectedBoxes.forEach((e) => {
+        e.textContent = "";
+      });
+    }
+
+    // Restart Function
+    const restartGame = () => {
+      xScore = [];
+      oScore = [];
+      checkArray(winCodes, xScore);
+      checkArray(winCodes, oScore);
+      resetGameBoardBoxes();
+      gameOptionsDiv.textContent = `${playersObj.player1} vs. ${playersObj.player2}`;
+    };
+
+    // Restart Button
+    const restartBtn = document.createElement("button");
+    restartBtn.type = "button";
+    restartBtn.className = "option-button";
+    restartBtn.textContent = "Restart";
+    restartBtn.addEventListener("click", restartGame);
+    body.append(restartBtn);
+
+    // New Game Section
+    const newGame = () => {
+      xScore = [];
+      oScore = [];
+      checkArray(winCodes, xScore);
+      checkArray(winCodes, oScore);
+      resetGameBoardBoxes();
+      gameOptionsDiv.textContent = "";
+      gameOptionsDiv.className = "game-options";
+      gameOptionsDiv.append(vsPlayerBtn);
+      // gameOptionsDiv.append(vsCpuBtn);
+    };
+
+    // New Game Button
+    const newGameBtn = document.createElement("button");
+    newGameBtn.type = "button";
+    newGameBtn.className = "option-button";
+    newGameBtn.textContent = "New Game";
+    newGameBtn.addEventListener("click", newGame);
+    body.append(newGameBtn);
 
   })();
 };
